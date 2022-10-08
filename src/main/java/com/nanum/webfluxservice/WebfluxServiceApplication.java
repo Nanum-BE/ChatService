874acf95252d26021bf42f28d135939cc40bd8c2
Chan <33cks1423@naver.com>
2022-10-08 15:57:31 +0900

    [#8] Error: build failed update

--- src/main/java/com/nanum/webfluxservice/WebfluxServiceApplication.java
+++ src/main/java/com/nanum/webfluxservice/WebfluxServiceApplication.java
@@ -2,12 +2,8 @@ package com.nanum.webfluxservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.nanum")
 @EnableReactiveMongoAuditing
--- src/main/java/com/nanum/webfluxservice/chat/infrastructure/ChatRepository.java
+++ src/main/java/com/nanum/webfluxservice/chat/infrastructure/ChatRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.mongodb.repository.Meta;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.data.mongodb.repository.Tailable;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
-
+@Repository
 public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
 
     @Tailable // 커서를 안닫고 계속 유지한다.
